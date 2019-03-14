34676a7e6c185ebb783bcd5497dcb85a8b6f80f5
Bowen Sun <nusnewob@users.noreply.github.com>
2019-03-14 10:46:52 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 pritunl set-mongodb ${MONGODB_URI}
 pritunl set app.reverse_proxy true
-pritunl set app.server_ssl ${SSL_ENABLED}
-pritunl set app.server_port ${WEB_PORT}
+pritunl set app.server_ssl ${SSL_ENABLED:-true}
+pritunl set app.server_port ${WEB_PORT:-443}
 
 exec $@
