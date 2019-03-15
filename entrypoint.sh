78c104bfe8afc89e3437c87ce335ee39824eb607
Bowen Sun <bowensun@gmail.com>
2019-03-15 14:06:14 +0000

    Revert "Update entrypoint.sh"
    This reverts commit 34676a7e6c185ebb783bcd5497dcb85a8b6f80f5.

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,7 +21,7 @@ EOF
 
 pritunl set-mongodb ${MONGODB_URI}
 pritunl set app.reverse_proxy true
-pritunl set app.server_ssl ${SSL_ENABLED:-true}
-pritunl set app.server_port ${WEB_PORT:-443}
+pritunl set app.server_ssl ${SSL_ENABLED}
+pritunl set app.server_port ${WEB_PORT}
 
 exec $@
