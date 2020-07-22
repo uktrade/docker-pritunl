6a03647c4fc0cebea04b67f599757a0ff5870bf4
Bowen Sun <bowensun@gmail.com>
2020-07-22 14:38:03 +0100

    update multiple hosts

--- scripts/update.py
+++ scripts/update.py
@@ -10,7 +10,11 @@ hosts = db.hosts
 servers = db.servers
 
 hosts.delete_many({'status': 'offline'})
-hosts.update_many({'status': 'online'}, {'$set': {'name': env('PRITUNL_HOST')}})
-host = hosts.find_one({'status': 'online'})
-result = servers.update_many({}, {'$set': {'hosts': [host['_id']]}})
+hosts.update_many({'status': 'online'}, {'$set': {'name': env('PRITUNL_HOST'), 'public_address': env('PRITUNL_DOMAIN')}})
+new_hosts = hosts.find({'status': 'online'})
+new_host_ids = []
+for host in new_hosts:
+    new_host_ids.append(host['_id'])
+
+result = servers.update_many({}, {'$set': {'hosts': new_host_ids}})
 print(result.modified_count)
